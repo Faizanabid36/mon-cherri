e61fc2a2da2ee22a4ba60b554a76d0d52f5fbc73
Usaid Alam <usaidalam469@gmail.com>
2021-06-13 17:51:02 +0500

    asd

--- app/Variation.php
+++ app/Variation.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Variation extends Model
 {
     protected $guarded=[];
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Product','product_variations','variation_id','product_id','');
+    }
 }
