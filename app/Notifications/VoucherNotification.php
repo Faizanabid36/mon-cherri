2b3f5b62fa5ebb4f43f14a34b13c22ec76fb1dce
Faizan Abid <faizanabid36@gmail.com>
2021-11-03 21:48:12 +0500

    removed-queue

--- app/Notifications/VoucherNotification.php
+++ app/Notifications/VoucherNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class VoucherNotification extends Notification implements ShouldQueue
+class VoucherNotification extends Notification
 {
     use Queueable;
 
